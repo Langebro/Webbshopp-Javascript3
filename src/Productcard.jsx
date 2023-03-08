050573fb2d1bb9e89125c8dbbb179c8d3804c56b
Nicolina Langebro <langebro92_@hotmail.com>
2023-03-08 20:12:52 +0100

    added functionality to make items add to cart

--- src/Productcard.jsx
+++ src/Productcard.jsx
@@ -1,14 +1,25 @@
 import "./App.css";
 import "./Productard.css";
+import { useContext, useState } from "react";
+import Productinfo from "./Productinfo";
+import { shopContext } from "./Context/Context";
 
 function Productcard(props) {
+  const { addToCart, cart, cartItems } = useContext(shopContext);
+  const [show, setShow] = useState(false);
+  const itemAmount = cartItems[props.productid];
+  const addToCartBtn = (e) => {
+    e.preventDefault();
+    addToCart(props.productid);
+  };
+
   return (
     <div className="productCard">
       <span id="name">{props.name}</span>
       <span>{props.price} SEK</span>
       <img src={props.img}></img>
       <span>
-        <button className="cartBtn">
+        <button className="cartBtn" onClick={addToCartBtn}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -20,10 +31,14 @@ function Productcard(props) {
             <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
             <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
           </svg>
+          {itemAmount > 0 && <>({itemAmount})</>}
         </button>
       </span>
       <span>
-        <a href="#">More information</a>
+        <a href="#" onClick={() => setShow(true)}>
+          More information
+        </a>
+        <Productinfo show={show} close={() => setShow(false)} product={props} />
       </span>
     </div>
   );
