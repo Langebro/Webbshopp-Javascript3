7c4323bd426ae98acaa24f5089a147799151f18e
Nicolina Langebro <langebro92_@hotmail.com>
2023-03-08 20:15:31 +0100

    Added a contextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Productcart from "./Productcart";
 import Searchfield from "./Searchfield";
+import { ShopContextProvider } from "./Context/Context";
 
 function App() {
   return (
     <div className="webbshoppApp">
       <h1>The Magic Store</h1>
-      <Searchfield />
+      <ShopContextProvider>
+        <Productcart />
+        <Searchfield />
+      </ShopContextProvider>
     </div>
   );
 }
