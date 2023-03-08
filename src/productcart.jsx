b04936ffd53333ebba9ad130078865f3ef70eaf8
Nicolina Langebro <langebro92_@hotmail.com>
2023-03-08 20:13:35 +0100

    added component with functionality for productcart

--- src/productcart.jsx
+++ src/productcart.jsx
@@ -1,8 +1,26 @@
+import data from "./data.json";
+import { useContext, useState } from "react";
+import { shopContext } from "./Context/Context";
+import Cartitem from "./Cartitem";
+import "./Productcart.css";
+
 function Productcart() {
+  const { cartItems } = useContext(shopContext);
   return (
-    <>
+    <div className="cart">
       <h3>Productcart</h3>
-    </>
+      <div className="cartItems">
+        {data.Products.map((product) => {
+          if (cartItems[product.productid] > 0) {
+            return (
+              <div className="cartlist" key={product.productid}>
+                <Cartitem {...product} />
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
   );
 }
 
