7920b82c609dbed2fb93fe572f1ee40b3da8544b
Nicolina Langebro <langebro92_@hotmail.com>
2023-03-14 15:06:32 +0100

    search is more responsive now

--- src/Searchfield.jsx
+++ src/Searchfield.jsx
@@ -5,25 +5,32 @@ import "./Searchfield.css";
 import Searchresult from "./Searchresult";
 
 function Searchfield() {
-  const [search, setSearch] = useState(""); // state för sökfäkt
+  const [search, setSearch] = useState(""); // state för sökfält
   const [products, setProducts] = useState(data.Products); // prroduktstate
 
   let onChange = (e) => {
-    setSearch(e.target.value);
-    if (search === "" || search === undefined) {
+    //
+    setSearch(e.target.value); // använder sökstaten för att få fram värde ur input
+    var searchValue = e.target.value;
+    if (searchValue === "" || searchValue === undefined) {
       setProducts(data.Products);
       return;
+    }
+    if (searchValue === "") {
+      return data.Products;
     }
 
     let changes =
       data.Products.filter((product) => {
-        if (product.name.toLowerCase().includes(search.toLowerCase())) {
-          return product;
+        // kollar om det som skrivs i sökfältet matchar någon av produkterna
+        if (product.name.toLowerCase().includes(searchValue.toLowerCase())) {
+          return product; // bokstäverna som skickas in matchar produktnamnet returneras produkt
         }
       }) ?? [];
 
     setProducts(changes);
   };
+
   return (
     <div className="searchWrapper">
       <div className="searchbar">
